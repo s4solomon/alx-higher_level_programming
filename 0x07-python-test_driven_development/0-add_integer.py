41b7e06615e302112e0521743cdeeab4a9b4b209
Solomon Iyanda <s4solomon4@gmail.com>
2023-04-05 10:06:49 -0700

    fixed 0 & 4

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,44 +1,22 @@
 #!/usr/bin/python3
 """
-Module to divide all elements of a matrix
+Module to find the sum of two integers or floats
 """
 
 
-def matrix_divided(matrix, div):
+def add_integer(a, b):
     """
-    Function to divide all elements of a matrix
-    and return results in new matrix
+    Function to find and return sum of two integers or floats
     """
 
-    """Checks if div exists"""
-    if div is None:
-        raise TypeError("div must be a number")
+    """Checks if first variable is an int or float"""
+    if not isinstance(a, int) and not isinstance(a, float) or \
+       isinstance(a, bool):
+        raise TypeError("a must be an integer")
 
-    """Checks for 0 division"""
-    if div == 0:
-        raise ZeroDivisionError("division by zero")
+    """Checks if second variable is an int or float"""
+    if not isinstance(b, int) and not isinstance(b, float) or \
+       isinstance(b, bool):
+        raise TypeError("b must be an integer")
 
-    """Checks if div is a number"""
-    if not isinstance(div, int) and not isinstance(div, float):
-        raise TypeError("div must be a number")
-
-    """Checks if row is a list"""
-    for row in matrix:
-        if type(row) is not list:
-            raise TypeError("matrix must be a matrix (list of lists) "
-                            "of integers/floats")
-
-    """Checks length of matrix rows"""
-    for row in matrix:
-        if len(row) != len(matrix[0]):
-            raise TypeError("Each row of the matrix must have the same size")
-
-    """Checks that matrix is made only of numbers"""
-    for row in matrix:
-        for i in range(len(row)):
-            if not isinstance(row[i], int) and not isinstance(row[i], float):
-                raise TypeError("matrix must be a matrix (list of lists) "
-                                "of integers/floats")
-
-    return [[round(row[i] / div, 2) for i in range(len(row))]
-            for row in matrix]
+    return int(a) + int(b)
--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -1,29 +1,14 @@
 #!/usr/bin/python3
-"""
-Module that prints text with 2 new lines after ., ?, and :
-"""
+"""Print square"""
 
 
-def text_indentation(text):
-    """
-    Function that prints text with 2 new lines after ., ?, and :
-    """
-
-    new_text = ""
-
-    """Checks if text is a string"""
-    if not isinstance(text, str):
-        raise TypeError("text must be a string")
-
-    """Checks if delimiters are in the text"""
-    if "." not in text and "?" not in text and ":" not in text:
-        print("{}".format(text))
-    else:
-        new_text = text.replace(". ", ".")
-        new_text = new_text.replace("? ", "?")
-        new_text = new_text.replace(": ", ":")
-        new_text = new_text.replace(".", ".\n\n")
-        new_text = new_text.replace("?", "?\n\n")
-        new_text = new_text.replace(":", ":\n\n")
-        " ".join(new_text.split())
-        print("{}".format(new_text), sep="", end="")
+def print_square(size):
+    """prints a square with the character #"""
+    if type(size) is not int:
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
+    for i in range(size):
+        for j in range(size):
+            print("#", end="")
+        print("")
