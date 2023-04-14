33cda213e127c9982e77f0da543423bcb5fee46b
Solomon Iyanda <s4solomon4@gmail.com>
2023-04-14 14:28:23 -0700

    mandatory tasks

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""
+Defines an empty class Square with size attribute
+"""
+
+class square:
+    """ Defines Square class with private size attribute """
+    def __init__(self, size):
+        self.__size = size
