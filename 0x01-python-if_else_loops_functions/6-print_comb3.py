ff15cb089949fcbd953db8ef1f7d52c95277c699
Solomon Iyanda <s4solomon4@gmail.com>
2023-11-28 15:58:03 -0800

    fixed task6

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
-for i in range(10):
-    for j in range(i + 1, 10):
-        print(f"{i:02d}, {j:02d}", end=', ' if i < 8 or j < 9 else '\n')
+for i in range(100):
+    print(f"{i:02d}", end=", " if i < 99 else "\n", flush=True)
